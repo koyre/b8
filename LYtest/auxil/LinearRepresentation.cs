26a36b279f61a29b0ef0b29a14b66e1897ab4bb3
Nikita Nezhevskii <empthynickname@gmail.com>
2017-03-16 12:39:15 +0300

    Добавлена заготовка для генерации 3х адресного кода

---
+++ LYtest/auxil/LinearRepresentation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LYtest.auxil
+{
+    public enum OperationType
+    {
+        Assign,
+        Plus,
+        Minus,
+        Mult,
+        Div
+    }
+
+    public class LinearRepresentation
+    {
+        // TODO
+    }
+
+}
