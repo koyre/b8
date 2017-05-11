1eacbcaa2be61ef3a1d3b5d4a3edadebaae42da5
Dmitry <DimonErmolaev1994@gmail.com>
2017-05-11 22:14:17 +0300

    Fix bug with only forward edges

--- LYtest/CFG/DepthSpanningTree.cs
+++ LYtest/CFG/DepthSpanningTree.cs
@@ -11,14 +11,12 @@ namespace LYtest.CFG
 {
     public class DepthSpanningTree
     {
-        public HashSet<CFGNode> Visited { get; }
         public Dictionary<CFGNode, int> Numbers { get; }
         public BidirectionalGraph<CFGNode, Edge<CFGNode>> SpanningTree { get; }
 
         public DepthSpanningTree(CFGraph cfg)
         {
             int numberOfVertices = cfg.num_of_vertexes - 1;
-            Visited = new HashSet<CFGNode>();
             SpanningTree = new BidirectionalGraph<CFGNode, Edge<CFGNode>>();
             Numbers = new Dictionary<CFGNode, int>();
             
@@ -29,7 +27,6 @@ namespace LYtest.CFG
         {
             if (node == null)
                 return;
-            Visited.Add(node);
             CFGLookup lookup = new CFGLookup(node);
 
             if (!lookup.CanMoveDirect() && !lookup.CanMoveGoto())
@@ -54,17 +51,14 @@ namespace LYtest.CFG
 
             foreach (var child in children)
             {
-                if (!Visited.Contains(child))
-                {
-                    if (!SpanningTree.Vertices.Contains(node))
-                        SpanningTree.AddVertex(node);
+                if (!SpanningTree.Vertices.Contains(node))
+                    SpanningTree.AddVertex(node);
 
-                    if (!SpanningTree.Vertices.Contains(child))
-                        SpanningTree.AddVertex(child);
+                if (!SpanningTree.Vertices.Contains(child))
+                    SpanningTree.AddVertex(child);
 
-                    SpanningTree.AddEdge(new Edge<CFGNode>(node, child));
-                    BuildTree(child, ref currentNumber);
-                }
+                SpanningTree.AddEdge(new Edge<CFGNode>(node, child));
+                BuildTree(child, ref currentNumber);
 
                 Numbers[node] = currentNumber;
                 currentNumber -= 1;
