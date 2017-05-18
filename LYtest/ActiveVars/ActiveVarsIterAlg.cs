679fb3633d6b4cd25109ac1e18a71a5e3e870bcd
maxial <alievmaxx@gmail.com>
2017-05-18 19:40:31 +0300

    active vars - iter alg

---
+++ LYtest/ActiveVars/ActiveVarsIterAlg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LYtest.CFG;
+using LYtest.IterAlg;
+using LYtest.LinearRepr.Values;
+
+namespace LYtest.ActiveVars
+{
+    public sealed class ActiveVarsIterAlg : IterativeCommonAlg<HashSet<LabelValue>>
+    {
+        protected override HashSet<LabelValue> Top => new HashSet<LabelValue>();
+
+        private readonly DefUseBuilder DefUse;
+
+        public ActiveVarsIterAlg(CFGraph g) : base(g)
+        {
+            DefUse = new DefUseBuilder(g.Blocks);
+            ReverseRun();
+        }
+
+        protected override bool ContCond(HashSet<LabelValue> a, HashSet<LabelValue> b)
+        {
+            return !a.SetEquals(b);
+        }
+
+        protected override HashSet<LabelValue> TransferFunc(CFGNode node)
+        {
+            var res = DefUse.UseLabels(node.Value);
+            res.UnionWith(Out[node].Except(DefUse.DefLabels(node.Value)));
+            return res;
+        }
+
+        protected override HashSet<LabelValue> MeetOp(List<CFGNode> nodes)
+        {
+            return new HashSet<LabelValue>(nodes.SelectMany(n => In[n]));
+        }
+    }
+}
