b89bab5148783d1e85060b1be86d96687c933fd5
Vdimir <vcherckasov@yandex.ru>
2017-05-20 23:33:32 +0300

    improve doc generation, add result to repo

--- docs/build.bat
+++ docs/build.bat
@@ -1,9 +1,14 @@
-SET trg=html
+rem SET PDF=pdf
+
 SET tmpfile=doc.md
 
 for %%f in (src\*.md) do type "%%f" >> %tmpfile% && echo. >> %tmpfile%
 
-pandoc -o dist\doc.%trg% %tmpfile% --latex-engine=xelatex -V mainfont="Calibri" --toc --toc-depth=4 -c github-pandoc.css
+if defined PDF (
+    pandoc --listings -H dist/listings-setup.tex -o dist\doc.pdf %tmpfile% --latex-engine=xelatex -V mainfont="Calibri" --toc --toc-depth=4
+) else (
+    pandoc -o dist\doc.html %tmpfile% -V mainfont="Calibri" --toc --toc-depth=4 -c github-pandoc.css
+)
 
 del %tmpfile%
 
