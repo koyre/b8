2db142a23aaf65f0450aaa8091fb40467f9bea4f
Stanislav Fateev <ssf94@yandex.ru>
2017-05-22 00:30:37 +0300

    Add class Region with varieties

---
+++ LYtest/Region/LoopRegion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LYtest.BaseBlocks;
+
+namespace LYtest.Region
+{
+    class LoopRegion : Region
+    {
+        List<Region> regions;
+        BodyRegion bodyReg;
+
+        public List<Region> Regions { get { return regions; } }
+        public BodyRegion BodyReg { get { return bodyReg; } }
+
+        public LoopRegion(BaseBlock header, List<BaseBlock> blocks, List<Region> regions) : base (header, blocks)
+        {
+            this.regions = regions;
+        }
+    }
+}
+}
